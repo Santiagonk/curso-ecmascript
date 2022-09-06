cfa83741682456dd6bfe9e264c1f6a2b8b4756f7
Santiago Salgado <jssalgadog@gmail.com>
2022-09-06 05:27:44 -0500

    spread operator

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -11,3 +11,29 @@ let user = { username: 'Santiago', age: 30};
 let {username, age} = user;
 
 console.log(username, user.age);
+
+// spread operator
+
+let person = {name: 'santiago', age: 20};
+let country = 'CO';
+
+let data = {id:1, ...person, country }
+
+console.log(data);
+
+// rest 
+
+function sum(num, ...values){
+    console.log(values);
+    console.log(num + values[0])
+    return num + values[0];
+}
+
+sum(1, 2, 3, 4, 5);
+
+// comment by partner
+let kissEmoji = [..."👩‍❤️‍💋‍👩"]
+console.log(kissEmoji)
+
+let familyEmoji = [..."👨‍👩‍👦‍👦"]
+console.log(familyEmoji)
